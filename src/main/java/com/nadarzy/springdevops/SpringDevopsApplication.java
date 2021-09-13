033ac2f7cef6e37f8b8bc975d3df7b3ebdb5da88
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-13 12:04:09 +0200

    jasypt working, encrypting manually (cli) + no special signs works

--- src/main/java/com/nadarzy/springdevops/SpringDevopsApplication.java
+++ src/main/java/com/nadarzy/springdevops/SpringDevopsApplication.java
@@ -1,11 +1,10 @@
 package com.nadarzy.springdevops;
 
-import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableEncryptableProperties
+// @EnableEncryptableProperties
 public class SpringDevopsApplication {
 
   public static void main(String[] args) {
